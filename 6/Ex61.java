f96c55e706d815b21d7a201b24fba4df489f8e58
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-08-26 19:28:46 +0900

    6-1~2

---
+++ 6/Ex61.java
@@ -0,0 +1,15 @@
+package kitakoubou;
+
+public class Ex61 {
+
+	public static void main(String[] args) {
+		// 処理結果の出力結果
+		System.out.println("3の2乗=" + x(3));
+	}
+
+	//2乗のメソッド
+	static int x(int n) {
+		//戻り値に2乗の計算式を設定する
+		return n * n;
+	}
+}
---
+++ 6/Ex62.java
@@ -0,0 +1,15 @@
+package kitakoubou;
+
+public class Ex62 {
+
+	public static void main(String[] args) {
+		// ここにコードを挿入
+		System.out.println("36と96の平均=" + average(36, 96));
+	}
+
+	//2つの整数の平均のメソッド
+	static int average(int x, int y) {
+		//戻り値に平均の計算式を設定する
+		return (x + y) / 2;
+	}
+}
